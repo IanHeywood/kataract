bfa3f5c2b6c6d1279487b8f007543c73920e622b
IanHeywood <ian.heywood@gmail.com>
2024-02-07 15:35:22 +0000

    Min/max modifications

--- kataplot.py
+++ kataplot.py
@@ -42,8 +42,8 @@ parser.add_option('--nostokes', dest = 'nostokes', help = 'Do not attempt to fin
 parser.add_option('--cmap', dest = 'mycmap', help = 'Colour map to use (default = gist_heat)', default = 'gist_heat')
 parser.add_option('--flagcol', dest = 'flagcol', help = 'Colour to use for flagged data (default = black)', default = 'black')
 parser.add_option('--interp', dest = 'interp', help = 'Image interpolation (default = bicubic)', default = 'bicubic')
-parser.add_option('--vmin', dest = 'vmin', help = 'Minimum value for colour scale (default = 0.0)', default = 0.0)
-parser.add_option('--vmax', dest = 'vmax', help = 'Maximum value for colour scale (default = half of peak value)', default = '')
+parser.add_option('--vmin', dest = 'vmin', help = 'Minimum value for colour scale (default = data minimum)', default = 0.0)
+parser.add_option('--vmax', dest = 'vmax', help = 'Maximum value for colour scale (default = data maximum)', default = '')
 parser.add_option('--tmin', dest = 'tmin', help = 'Minimum of time range (data units, default = minimum)', default = '')
 parser.add_option('--tmax', dest = 'tmax', help = 'Maximum of time range (data units, default = maximum)', default = '')
 parser.add_option('--fmin', dest = 'fmin', help = 'Minimum of frequency range (data units, default = minimum)', default = '')
@@ -59,7 +59,7 @@ nostokes = options.nostokes
 mycmap = options.mycmap
 flagcol = options.flagcol
 interp = options.interp
-vmin = float(options.vmin)
+vmin = options.vmin
 vmax = options.vmax
 tmin = options.tmin
 tmax = options.tmax
@@ -81,7 +81,8 @@ else:
 # Find all the other Stokes params (or not)
 if not nostokes:
 	to_plot = get_files(datfile)
-
+else:
+	to_plot = [datfile]
 
 # Set up colour map and NaN colour
 cmap = mpl.cm.get_cmap(mycmap).copy()
@@ -96,12 +97,21 @@ for datfile in to_plot:
 	pngname = datfile+'.png'
 	print('Plotting Stokes %s (%s)' % (corr,datfile)) 
 
-	# Sort out vmax if not supplied by user
+	raw = data[corr].values
+	datamin = numpy.min(raw[~numpy.isnan(raw)])
+	datamax = numpy.max(raw[~numpy.isnan(raw)])
+	print('   Data min: '+str(datamin))
+	print('   Data max: '+str(datamax))
+
+	# Sort out vmax/vmin if not supplied by user
 	if vmax != '':
 		vmax = float(vmax)
 	else:
-		raw = data[corr].values
-		vmax = 0.5*numpy.max(raw[~numpy.isnan(raw)])
+		vmax = datamax
+	if vmin != '':
+		vmin = float(vmin)
+	else:
+		vmin = datamin
 
 	# Frequency plot limits
 	if fmin == '':
